b723ae630628b132f9122cd5606dfd7005d91d26
Simon Blanco <simonblanco@protonmail.com>
2022-12-15 00:17:31 +0100

    Implemented list comprehension to format crates and instructions

--- Day5/1_part.py
+++ Day5/1_part.py
@@ -4,55 +4,54 @@ Process text:
 
 Read top crate
 Move top crate (origin, dest, times)
-
-#Split input in 2 files
-with open ("sampleinput.txt", "r") as input:
-    for line in input:
-        if line.startswith('move'):
-            with open ("instructions.txt", "a") as instructions_txt:
-                instructions_txt.write(line)
-        else:
-            with open ("crates.txt", "a") as crates_txt:
-                crates_txt.write(line)
-
-def ReadTopCrate():
-    print(instr_numeric)
-    print (crates[0])
-    print (crates, instructions)
-
 """
 
-
 import re
+from itertools import zip_longest
 
 
 def MoveTopCrate (times, origin, dest):
     pass
 
-def CreateCrateMatrix(crates):
-#   Return a clean list with only numbers and letters. 
-#   Reverse the list so the numbers come first
-    new_crates = []
-    for crate in crates:
-        new_crate = re.sub(" {4}", empty_space, crate)
-        remove = "[]"
-        for character in remove:
-            new_crate = new_crate.replace(character, "")
-        new_crates.append(''.join(new_crate.split()))
-    new_crates.pop()
-    new_crates.reverse()
-    print(new_crates)
-    return new_crates
+def FormatCrates(crates):
+    # Format crates into a list of lists of columns, converting empty spaces into 'X'
+    crates = [re.sub(" {4}", empty_space, crate) for crate in crates] 
+    crates = [re.sub("\[|\]", "", crate) for crate in crates]
+    crates = [re.sub("X", "", crate) for crate in crates]
+    crates = [''.join(crate.split()) for crate in crates]
+    crates.pop()
+    crates.reverse()
+    return crates
 
 def CreateColumns(crate_list):
-    print(crate_list)
-    l2 = [list(x) for x in zip(*crate_list)]
-    print(l2)
+#    print(crate_list)
+    crate_columns = [list(x) for x in zip_longest(*crate_list)]
+#    print(crate_columns)
+    return crate_columns
+
+
+def MoveCrates(crates, instructions):
+    for inst in instructions:
+        times = inst[0]
+        origin = inst[1]
+        dest = inst[2]
+        buffer = None
+        #print(origin)
+        # do the following with list comp -> buffer = [.....
+        for crate in crates:
+            if crate[0] == origin:
+                print(crate)
+                while buffer == None:
+                    buffer = crate[-1]
+                    crate.pop()
+                print(crate)
+
+
 
 instructions = []
 crates = []
 instr_numeric = []
-empty_space = "[X]"
+empty_space = "X"
 
 with open ("sampleinput.txt", "r") as input:
     for line in input:
@@ -61,19 +60,19 @@ with open ("sampleinput.txt", "r") as input:
         else:
             crates.append(line)
 
-print (crates, instructions)
+#print (crates, instructions)
 
-"""
-# Lets put all the numbers in the instructions in a list.
-for instruction in instructions:
-    for char in instruction:
-        if char.isdigit():
-            instr_numeric.append(char)
-"""
-
+# Format instructions into a list of lists of numbers
 #new_instructions = [[char for instruction in instructions] for char in instruction if char.isdigit()] ==> ['1', '2', '1', '3', '1', '3', '2', '2', '1', '1', '1', '2']
 
-new_instructions = [[char for char in instruction if char.isdigit()] for instruction in instructions] 
-print(new_instructions)
-#crate_matrix=CreateCrateMatrix(crates)
-#CreateColumns(crate_matrix)
+instructions = [[char for char in instruction if char.isdigit()] for instruction in instructions]
+print(instructions)
+
+crates = FormatCrates(crates)
+#print(crates)
+
+crates = CreateColumns(crates)
+print(crates)
+
+MoveCrates(crates, instructions)
+
