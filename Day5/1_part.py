d2e6b36c56ce07bced275fec37b08fec667024c1
Simon Blanco <simonblanco@protonmail.com>
2022-12-19 18:45:48 +0100

    Finished day 5 part 1

--- Day5/1_part.py
+++ Day5/1_part.py
@@ -1,78 +1,71 @@
-"""
-Process text:
-    Create separate files for instructions and crates
-
-Read top crate
-Move top crate (origin, dest, times)
-"""
-
 import re
 from itertools import zip_longest
 
-
-def MoveTopCrate (times, origin, dest):
-    pass
+"""
+START FUNCTIONS
+"""
 
 def FormatCrates(crates):
-    # Format crates into a list of lists of columns, converting empty spaces into 'X'
-    crates = [re.sub(" {4}", empty_space, crate) for crate in crates] 
-    crates = [re.sub("\[|\]", "", crate) for crate in crates]
-    crates = [re.sub("X", "", crate) for crate in crates]
-    crates = [''.join(crate.split()) for crate in crates]
-    crates.pop()
-    crates.reverse()
+    # Format crates into a list of rows, converting empty spaces into 'X'
+    # The idea is creating a grid replacing the empty spaces for X in order to easily rearrange it
+    crates = [re.sub(" {4}", "X", crate) for crate in crates]       # replace 4 paces for X
+    crates = [re.sub("\[|\]", "", crate) for crate in crates]       # remove square brackets
+    crates = [''.join(crate.split()) for crate in crates]           # remove all spaces
+    crates.pop()                                                    # remove the \n
+    crates.reverse()                                                # put numbers first
     return crates
 
 def CreateColumns(crate_list):
-#    print(crate_list)
+    # combine the nth char of each string of the list to make columns
     crate_columns = [list(x) for x in zip_longest(*crate_list)]
-#    print(crate_columns)
+    # remove the X
+    crate_columns = [[x for x in crate if x != 'X'] for crate in crate_columns]
     return crate_columns
 
 
 def MoveCrates(crates, instructions):
+    # crates: list of colums (each column is a list starting with the index and followed by the crates, bottom to top)
     for inst in instructions:
         times = inst[0]
         origin = inst[1]
         dest = inst[2]
-        buffer = None
-        #print(origin)
-        # do the following with list comp -> buffer = [.....
-        for crate in crates:
-            if crate[0] == origin:
-                print(crate)
-                while buffer == None:
-                    buffer = crate[-1]
-                    crate.pop()
-                print(crate)
+        # crate[0][0] is the index, look for origin and destination
+        cr_origin = next(crate for crate in crates if crate[0][0] == origin)
+        cr_dest = next(crate for crate in crates if crate[0][0] == dest)
+        # move the crates
+        for i in range(int(times)):
+            if not cr_origin[-1].isdigit():         # make sure not moving the index
+                cr_dest.append(cr_origin[-1])
+                cr_origin.pop()
+    return crates
 
+def PrintTopCrate(crates):
+    top_crates = [crate[-1] for crate in crates]                # put the top crat of each column in a list
+    print(top_crates)
 
+"""
+END FUNCTIONS
+"""
 
 instructions = []
 crates = []
-instr_numeric = []
-empty_space = "X"
 
-with open ("sampleinput.txt", "r") as input:
+with open ("input.txt", "r") as input:
     for line in input:
         if line.startswith("move"):
             instructions.append(line)
         else:
             crates.append(line)
 
-#print (crates, instructions)
+print (crates, instructions)
 
-# Format instructions into a list of lists of numbers
-#new_instructions = [[char for instruction in instructions] for char in instruction if char.isdigit()] ==> ['1', '2', '1', '3', '1', '3', '2', '2', '1', '1', '1', '2']
-
-instructions = [[char for char in instruction if char.isdigit()] for instruction in instructions]
-print(instructions)
+# Format instructions into a list of lists of numbers (remove the words)
+instructions = [re.findall(r'\d+', instruction) for instruction in instructions]
 
 crates = FormatCrates(crates)
-#print(crates)
 
 crates = CreateColumns(crates)
-print(crates)
 
-MoveCrates(crates, instructions)
+crates = MoveCrates(crates, instructions)
 
+PrintTopCrate(crates)
