fab5f3b2c5fef75e7d22dedd3696cae3f0b43175
Simon Blanco <simonblanco@protonmail.com>
2022-12-20 11:33:23 +0100

    Solved part 2 of Day 6

---
+++ Day6/2_part.py
@@ -0,0 +1,34 @@
+#Day 6 Part 1
+
+# Check if there are repeated chars in the given chunk
+# If no repeated chars, return False to stop the loop in main
+def CheckChunk(chunk):
+    char_count = {}
+    for char in chunk:
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+    for char, count in char_count.items():
+        if count > 1:
+            return True
+    return False
+
+"""
+End of Functions
+"""
+
+
+with open ("input.txt", 'r') as input:
+    datastream = input.read()
+
+# Same process than in part 1, just change the offset (+10) and the slice.
+count = 13
+condition = True
+while condition:
+    chunk = datastream[:14]
+    print(chunk)
+    condition = CheckChunk(chunk)
+    count += 1
+    datastream = datastream[1:]
+print(count)
