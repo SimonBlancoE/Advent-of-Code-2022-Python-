5636eb4990a68114cddb873aeb3506655202daef
Simon Blanco <simonblanco@protonmail.com>
2023-02-15 18:08:21 +0100

    Solved Day 8 Part 1 (After Refactoring)

--- Day8/day8.py
+++ Day8/day8.py
@@ -6,40 +6,21 @@ with open('input', 'r') as file:
 data = np.array(data)
 
 
-def look_north(index, item):
-    print("looking north: ", data[:index[0], index[1]:index[1] + 1])
-    if np.any(item <= data[:index[0], index[1]:index[1] + 1]):
-        return False
-    else:
-        return True
+def tree_is_visible(index, item):
+    trees_north = data[:index[0], index[1]:index[1] + 1]
+    trees_east = data[index[0]:index[0] + 1, index[1] + 1:]
+    trees_west = data[index[0]:index[0] + 1, index[1] - 1::-1]
+    trees_south = data[index[0] + 1:, index[1]: index[1] + 1]
+    trees_in_axis = [trees_north, trees_east, trees_west, trees_south]
+    trees_in_axis = [np.squeeze(trees) for trees in trees_in_axis]
+
+    for trees in trees_in_axis:
+        if np.all(item > trees):
+            return True
+    return False
 
 
-def look_east(index, item):
-    print("looking east: ", data[index[0]:index[0] + 1, index[1] + 1:])
-    if np.any(item <= data[index[0]:index[0] + 1, index[1] + 1:]):
-        return False
-    else:
-        return True
-
-
-def look_west(index, item):
-    print("looking west: ", data[index[0]:index[0] + 1, index[1] - 1::-1])
-    if np.any(item <= data[index[0]:index[0] + 1, index[1] - 1::-1]):
-        return False
-    else:
-        return True
-
-
-def look_south(index, item):
-    print("looking south: ", data[index[0] + 1:, index[1]: index[1] + 1])
-    if np.any(item <= data[index[0] + 1:, index[1]: index[1] + 1]):
-        return False
-    else:
-        return True
-
-
-
-def exclude(index):
+def exclude_from_comparison(index):
     if index[0] == 0 or index[1] == 0 or index[1] == data.shape[1] - 1 or index[0] == data.shape[0] - 1:
         return True
 
@@ -47,16 +28,12 @@ def exclude(index):
 def count_visible_trees():
     count = 0
     for index, item in np.ndenumerate(data):
-        if not exclude(index):
-            print(index, item)
-            if look_north(index, item) or look_south(index, item) or look_east(index, item) or look_west(index, item):
+        if not exclude_from_comparison(index):
+            if tree_is_visible(index, item):
                 count += 1
-                print(f"Tree at position{index} with value {item} was counted")
         else:
             count += 1
-            print(f"Tree at position{index} was counted for being at the perimeter")
     return count
 
 
 print(count_visible_trees())
-
