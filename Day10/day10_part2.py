54d23c5e4f9a83491584fea06c9c75cd15959b45
Simon Blanco <simonblanco@protonmail.com>
2023-03-04 00:07:02 +0100

    Day 10 Part 1 Initial commit

--- Day10/day10_part2.py
+++ Day10/day10_part2.py
@@ -5,15 +5,36 @@ dot = '.'
 sprite_list = [hashtag if i < 3 else dot for i in range(40)]
 print(sprite_list)
 sprite_pos = 1
-cycle = []
+solution = []
+end_of_row = [40, 80, 120, 160, 200, 240]
+cycle = 1
+crt_row = []
+crt_pixel = 0
 for row in inp:
     try:
         _, n = row.split()
-        sprite_pos += int(n)
-        sprite_list = [hashtag if i in [sprite_pos - 1, sprite_pos, sprite_pos + 1] else dot for i in range(40)]
-        print(f"add {n}")
-        print(sprite_list)
-        for _ in range(2):
-            pass
     except ValueError:      # noop
-        pass
+        n = 0
+    finally:
+        if cycle == any(end_of_row):
+            solution.append(crt_row)
+            crt_row.clear()
+            crt_pixel = 0
+        if n:
+            sprite_pos += int(n)
+            for _ in range(2):
+                if sprite_list[crt_pixel] == hashtag:
+                    crt_row.append(hashtag)
+                else:
+                    crt_row.append(dot)
+                crt_pixel += 1
+            sprite_list = [hashtag if i in [sprite_pos - 1, sprite_pos, sprite_pos + 1] else dot for i in range(40)]
+        else:            # CRT draws pixel at next position
+            if sprite_list[crt_pixel] == hashtag:
+                crt_row.append(hashtag)
+            else:
+                crt_row.append(dot)
+            crt_pixel += 1
+
+for row in solution:
+    print(row)
